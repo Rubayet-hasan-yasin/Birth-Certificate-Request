eebde12feffd2d03746d2442ceb2dbfa44226d1a
Rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2024-01-27 20:43:09 +0600

    data save with csharp

--- Birth Certificate Request/Controllers/BirthCertificateController.cs
+++ Birth Certificate Request/Controllers/BirthCertificateController.cs
@@ -1,6 +1,8 @@
 ﻿using Birth_Certificate_Request.Models;
+using Birth_Certificate_Request.Models.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
 using System.Buffers.Text;
 
 namespace Birth_Certificate_Request.Controllers
@@ -10,18 +12,141 @@ namespace Birth_Certificate_Request.Controllers
     public class BirthCertificateController : ControllerBase
     {
 
+        [HttpGet]
+        public ActionResult GetCertificateData([FromQuery] long BRNumber)
+        {
+            //Console.WriteLine(BRNumber);
+
+            
+
+
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                Database = "BirthCertificate",
+                UserID = "root",
+                Password = "password1234",
+                SslMode = MySqlSslMode.None
+            };
+
+            var connection = new MySqlConnection(builder.ConnectionString);
+
+            
+
+            connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = $"SELECT * FROM birthdata WHERE BRNumber = {BRNumber}";
+
+            command.ExecuteNonQuery();
+
+            var reader = command.ExecuteReader();
+
+            BirthCertificateDTO data = new BirthCertificateDTO();
+
+            while (reader.Read())
+            {
+                int count = reader.FieldCount;
+
+                
+
+                for(int i = 0; i<count; i++)
+                {
+
+                    var value = reader.GetValue(i);
+
+                    Console.WriteLine(value);
+
+                    //data[i] = value;
+                    
+                }
+
+            }
+
+            connection.Close();
+
+
+            return Ok(data);
+
+        }
+
         [HttpPost]
-        public ActionResult<BirthCertificate> PostBirthCertificate([FromBody] BirthCertificate data)
+        public async void PostBirthCertificate([FromBody] BirthCertificate data)
         {
 
-            if(Base64.IsValid(data.ImageData))
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                Database= "BirthCertificate",
+                UserID= "root",
+                Password= "password1234",
+                SslMode = MySqlSslMode.None
+            };
+
+
+            var connection = new MySqlConnection(builder.ConnectionString);
+
+            await connection.OpenAsync();
+
+            // create a DB command and set the SQL
+            var command = connection.CreateCommand();
+            command.CommandText = "CREATE TABLE IF NOT EXISTS BirthData(" +
+                "RegisterNo INT NOT NULL," +
+                "DateOfIssue DATE,"+
+                "DateOfRegistration DATE,"+
+                "BRNumber BIGINT,"+
+                "Name VARCHAR(30)," +
+                "DateOfBirth DATE," +
+                "PlaceOfBirth VARCHAR(70)," +
+                "ImageData MEDIUMTEXT," +
+                "PermanentAddress VARCHAR(100)," +
+                "FatherName VARCHAR(50),"+
+                "FatherBRN BIGINT," +
+                "FatherNIDNumber BIGINT,"+
+                "FatherNationality VARCHAR(30),"+
+                "MotherName VARCHAR(50),"+
+                "MotherBRN BIGINT,"+
+                "MotherNIDNumber BIGINT,"+
+                "MotherNationality VARCHAR(30)"+
+                ");";
+
+            command.ExecuteNonQuery();
+
+            command.CommandText = "INSERT INTO BirthData VALUES(@RegisterNo, @DateOfIssue, @DateOfRegistration, @BRNumber, @Name, @DateOfBirth, @PlaceOfBirth, @ImageData, @PermanentAddress, @FatherName, @FatherBRN, @FatherNIDNumber, @FatherNationality, @MotherName, @MotherBRN, @MotherNIDNumber, @MotherNationality)";
+
+            command.Parameters.AddWithValue("RegisterNo", data.RegisterNo);
+            command.Parameters.AddWithValue("DateOfIssue", data.DateOfIssue);
+            command.Parameters.AddWithValue("DateOfRegistration", data.DateOfRegistration);
+            command.Parameters.AddWithValue("BRNumber", data.BRNumber);
+            command.Parameters.AddWithValue("Name", data.Name);
+            command.Parameters.AddWithValue("DateOfBirth", data.DateOfBirth);
+            command.Parameters.AddWithValue("PlaceOfBirth", data.PlaceOfBirth);
+            command.Parameters.AddWithValue("ImageData", data.ImageData);
+            command.Parameters.AddWithValue("PermanentAddress", data.PermanentAddress);
+            command.Parameters.AddWithValue("FatherName", data.FatherName);
+            command.Parameters.AddWithValue("FatherBRN", data.FatherBRN);
+            command.Parameters.AddWithValue("FatherNIDNumber", data.FatherNIDNumber);
+            command.Parameters.AddWithValue("FatherNationality", data.FatherNationality);
+            command.Parameters.AddWithValue("MotherName", data.MotherName);
+            command.Parameters.AddWithValue("MotherBRN", data.MotherBRN);
+            command.Parameters.AddWithValue("MotherNIDNumber", data.MotherNIDNumber);
+            command.Parameters.AddWithValue("MotherNationality", data.MotherNationality);
+            
+
+
+            int result = await command.ExecuteNonQueryAsync();
+
+            Console.WriteLine(result);
+
+
+            await connection.CloseAsync();
+
+            if (Base64.IsValid(data.ImageData))
             {
                 
 
-                return Ok(true);
+                Console.WriteLine("image data Base64");
             }
-            
-                return Ok(data);
+
 
         }
     }
---
+++ Birth Certificate Request/Models/DTO/BirthCertificateDTO.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Birth_Certificate_Request.Models.DTO
+{
+    public class BirthCertificateDTO
+    {
+        // registerNo
+        
+        public int RegisterNo { get; set; }
+
+
+        //date Of Issue
+        [DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString= "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfIssue { get; set; }
+
+
+        //Date of Registration
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfRegistration { get; set; }
+
+
+        // Birth Registration Number
+       
+        [Range(10000000000000000, 99999999999999999, ErrorMessage = "BR Number must be 17 digits")]
+        public long BRNumber { get; set; }
+
+
+        //Name 
+        [MinLength(3)]
+        public string Name { get; set; } = string.Empty;
+
+
+        //Date of Birth
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        //Place Of Birth
+        public string PlaceOfBirth { get; set; } = string.Empty;
+
+
+
+        //image in base64                SGVsbG8sIHRoaXMgaXMgYSBiYXNlNjQtZW5jb2RlZCBzdHJpbmcu
+        [Base64String(ErrorMessage = "Image data is must be a valid base64 string")]
+        public string ImageData { get; set; } = string.Empty;
+
+        //Permanent Address
+        [MinLength(5)]
+        public string PermanentAddress { get; set; } = string.Empty;
+
+
+        //Father's Name
+        public string FatherName { get; set; } = string.Empty;
+
+        //Father's Birth Registration Number
+        public long? FatherBRN { get; set; }
+
+        //Father's NID number
+        public long? FatherNIDNumber { get; set; }
+
+
+        //Father's Nationality
+        public string FatherNationality { get; set; } = string.Empty;
+
+
+
+
+        //Mother's Name
+        public string MotherName { get; set; } = string.Empty;
+
+        //Mother's Birht Registration Number
+        public long? MotherBRN { get; set; }
+
+
+        //Mother's NID Number
+        public long? MotherNIDNumber { get; set; }
+
+
+        //Mother's Nationality
+        public string MotherNationality { get; set; } = string.Empty;
+    }
+}
